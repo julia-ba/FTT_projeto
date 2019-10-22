e2fd67c6e0c95005f110a26a9d53cf20da80d30f
Júlia Botelho Alcântara <julia.alcantara@ftt-09L.engcomp.local>
2019-10-22 17:47:30 -0300

    Criando as classes 'Usuário' e 'Endereço'

--- demo/src/main/java/ftt/com/br/unievangelica/demo/domain/endereco/Endereco.java
+++ demo/src/main/java/ftt/com/br/unievangelica/demo/domain/endereco/Endereco.java
@@ -1,4 +1,64 @@
 package ftt.com.br.unievangelica.demo.domain.endereco;
 
-public class Endereco {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import ftt.com.br.unievangelica.demo.core.entity.AbstractEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/** Classe Endereço com lombok importado fazendo getter e setter **/
+@Getter
+@Setter
+@Entity
+@Table(name = "endereco")
+public class Endereco extends AbstractEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @Max(value = 8)
+    @Column (name ="cep" , nullable = false)
+    private String cep;
+
+    @NotNull
+    @Min(value = 3)
+    @Column (name ="rua" , nullable = false)
+    private String rua;
+
+    @NotNull
+    @Min(value = 3)
+    @Column (name ="numero" , nullable = false)
+    private Integer numero;
+
+    @NotEmpty
+    @Min(value = 3)
+    @Column (name ="bairo" , nullable = false)
+    private String bairro;
+
+    @NotEmpty
+    @Min(value = 3)
+    @Column (name ="cidade" , nullable = false)
+    private String cidade;
+
+    @NotEmpty
+    @Min(value = 2)
+    @Max(value = 2)
+    @Column (name ="estado" , nullable = false)
+    private String estado;
+
 }
--- demo/src/main/java/ftt/com/br/unievangelica/demo/domain/usuario/Usuario.java
+++ demo/src/main/java/ftt/com/br/unievangelica/demo/domain/usuario/Usuario.java
@@ -1,4 +1,66 @@
 package ftt.com.br.unievangelica.demo.domain.usuario;
 
-public class Usuario {
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import ftt.com.br.unievangelica.demo.core.entity.AbstractEntity;
+import ftt.com.br.unievangelica.demo.domain.endereco.Endereco;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+/** Classe Usuário com lombok importado fazendo getter e setter **/
+@Getter
+@Setter
+@Entity
+@Table(name = "usuario")
+public class Usuario extends AbstractEntity{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotEmpty /** NotEmpty usado apenas para strings **/
+    @Min(value = 3)
+    @Max(value = 50)
+    @Column (name ="nome" , nullable = false)
+    private String nome;
+
+    @NotEmpty
+    @Min(value = 3)
+    @Max(value = 50)
+    @Column (name ="sobrenome" , nullable = false)
+    private String sobrenome;
+
+    @NotNull /** Quando temos números é preferível usar NotNull **/
+    @Max(value = 8)
+    @Column (name ="dataNascimento" , nullable = false)
+    private Date dataNascimento;
+
+    @NotNull
+    @Min(value = 11)
+    @Max(value = 11)
+    @Column (name ="telefone" , nullable = false)
+    private String telefone;
+
+    @NotNull
+    @Min(value = 11)
+    @Max(value = 14)
+    @Column (name ="cpfCnpj" , nullable = false)
+    private String cpfCnpj;
+
+
+    /**
+     * Referenciando o ID da tabela Endereço
+     */
+    @OneToOne(fetch = FetchType.EAGER) /** Relação de OneToOne entre Usuário e Endereço **/
+    @JoinColumn(name = "endereco_id", referencedColumnName = "id")
+    private Endereco endereco;
+
 }
