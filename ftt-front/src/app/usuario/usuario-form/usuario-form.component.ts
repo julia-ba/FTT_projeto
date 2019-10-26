e7f012c9e393cd160a14a83f6381a9495c01266b
Lucas Pereira <lucasp28.contato@gmail.com>
2019-10-26 15:43:36 -0300

    adicionando validacao de cpf e arrumando a lista

--- ftt-front/src/app/usuario/usuario-form/usuario-form.component.ts
+++ ftt-front/src/app/usuario/usuario-form/usuario-form.component.ts
@@ -1,10 +1,127 @@
 import {Component, OnInit} from '@angular/core';
 
-import {FormGroup, Validators, FormBuilder} from '@angular/forms';
+import {FormGroup, Validators, FormBuilder, FormControl} from '@angular/forms';
 import { Usuario } from '../usuario';
 import Validation from 'src/app/core/util/validation';
 import { UsuarioService } from '../usuario.service';
 import { Router } from '@angular/router';
+
+function validacaoCpfCnpj(control: FormControl){
+  const cpfCnpj = control.value
+  
+  if(cpfCnpj.length === 14){
+    return validarCPF(cpfCnpj) ? null : {
+      cpfError: {
+        cpfCnpj
+      }
+    }
+  }else if(cpfCnpj.length === 18){
+    return validarCnpj(cpfCnpj) ? null : {
+      cnpjError: {
+        cpfCnpj
+      }
+    }
+  }else{
+    return {
+      cpfCnpjError:{
+        cpfCnpj  
+      }
+    }
+  }
+
+}
+
+function validarCPF(cpf) {
+  cpf = cpf.replace(/[^\d]+/g, '');
+  if (cpf === '') {
+    return false;
+  }
+  if (cpf.length !== 11 ||
+    cpf === '00000000000' ||
+    cpf === '11111111111' ||
+    cpf === '22222222222' ||
+    cpf === '33333333333' ||
+    cpf === '44444444444' ||
+    cpf === '55555555555' ||
+    cpf === '66666666666' ||
+    cpf === '77777777777' ||
+    cpf === '88888888888' ||
+    cpf === '99999999999') {
+    return false;
+  }
+  let add = 0;
+  for (let i = 0; i < 9; i++) {
+    add += parseInt(cpf.charAt(i)) * (10 - i);
+  }
+  let rev = 11 - (add % 11);
+  if (rev === 10 || rev === 11) {
+    rev = 0;
+  }
+  if (rev !== parseInt(cpf.charAt(9))) {
+    return false;
+  }
+  add = 0;
+  for (let i = 0; i < 10; i++) {
+    add += parseInt(cpf.charAt(i)) * (11 - i);
+  }
+  rev = 11 - (add % 11);
+  if (rev === 10 || rev === 11) {
+    rev = 0;
+  }
+  if (rev !== parseInt(cpf.charAt(10))) {
+    return false;
+  }
+
+  return true;
+}
+
+function validarCnpj(cnpj) {
+  cnpj = cnpj.replace(/[^\d]+/g, '');
+  if (!cnpj || cnpj.length !== 14
+    || cnpj === '00000000000000'
+    || cnpj === '11111111111111'
+    || cnpj === '22222222222222'
+    || cnpj === '33333333333333'
+    || cnpj === '44444444444444'
+    || cnpj === '55555555555555'
+    || cnpj === '66666666666666'
+    || cnpj === '77777777777777'
+    || cnpj === '88888888888888'
+    || cnpj === '99999999999999') {
+    return false;
+  }
+  let tamanho = cnpj.length - 2;
+  let numeros = cnpj.substring(0, tamanho);
+  const digitos = cnpj.substring(tamanho);
+  let soma = 0;
+  let pos = tamanho - 7;
+  for (let i = tamanho; i >= 1; i--) {
+    soma += numeros.charAt(tamanho - i) * pos--;
+    if (pos < 2) {
+      pos = 9;
+    }
+  }
+  let resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+  if (resultado != digitos.charAt(0)) {
+    return false;
+  }
+  tamanho++;
+  numeros = cnpj.substring(0, tamanho);
+  soma = 0;
+  pos = tamanho - 7;
+  for (let i = tamanho; i >= 1; i--) {
+    soma += numeros.charAt(tamanho - i) * pos--;
+    if (pos < 2) {
+      pos = 9;
+    }
+  }
+  resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+  if (resultado != digitos.charAt(1)) {
+    console.log(cnpj);
+    return false;
+  }
+  return true;
+}
 
 
 @Component({
@@ -26,8 +143,8 @@ export class UsuarioFormComponent implements OnInit {
       nome: ['', [Validators.required, Validators.minLength(3)]],
       sobrenome: ['', [Validators.required, Validators.minLength(3)]],
       dataNascimento: ['', [Validators.required, Validators.maxLength(10)]],
-      telefone: ['', [Validators.required, Validators.maxLength(11), Validators.minLength(11)]],
-      cpfCnpj: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+      telefone: ['', [Validators.required, Validators.pattern(/^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/)]],
+      cpfCnpj: ['', [Validators.required, Validators.minLength(14), Validators.maxLength(18)]],
       endereco: this.builder.group({
         id:[], 
         cep: ['', [Validators.required, Validators.maxLength(8)]],
@@ -38,7 +155,7 @@ export class UsuarioFormComponent implements OnInit {
         estado: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]]   
       })      
     }, {});
-    
+    console.log(this.usuarioForm);
   }
   async onSubmit(usuario: Usuario) {
     if (this.usuarioForm.invalid) {
@@ -57,7 +174,7 @@ export class UsuarioFormComponent implements OnInit {
       }else {
         this.usuarioService.save(usuario)
         .subscribe(usuario => {
-          console.log("Usuario salva!!!");
+          console.log("Usuario salvo!!!");
   
           //Redireciona para a lista de usuario --> alterar aq dps da listagem
           this.router.navigate(['/usuario']); // <--
